987be084754b5721053fa57d516fc6e9fbb176c2
Ivanclj <33612133+Ivanclj@users.noreply.github.com>
2017-11-16 20:34:06 -0500

    Add files via upload

--- Projectver1.R
+++ Projectver1.R
@@ -1,22 +1,40 @@
-mydata = read.csv("C:/FALL 2017/STAT 545/STAT 545 Project/train.csv")
-mydata
-names<-mydata[,4]
-#names_characterized<-as.character(names[800])
-#splitted_names<-strsplit(names_characterized,",")
-splitted_names<-c()
+mydata = read.csv("mydata.csv")
+str(mydata)
+library('ggplot2') # visualization
+library('tidyverse')
+dim(mydata)
+str(mydata) 
+summary(mydata)
+sum(mydata$Cabin=="")
 
-for(i in 1:length(names)){
-  names_characterized<-as.character(names[i])
-  splitted_name<-strsplit(names_characterized,",")
-  list5<-splitted_name[[1]]
-  list6<-list5[1]
-  splitted_names[i]<-list6
-  splitted_names[i]
+#people with same ticket are from same group
+mydata<-mutate(mydata, grp=rep(0,nrow(mydata)))#grp number, 0 =>single
+uniq_ticket<-table(mydata$Ticket)
+not_single<-uniq_ticket[uniq_ticket>1]
+for(ii in 1:length(not_single)){
+  this_ticket<-names(not_single)[ii]
+  tickt_index<-which(mydata$Ticket==this_ticket)
+  mydata$grp[tickt_index]<-ii
 }
+mydata %>% group_by(grp) %>% summarise(sur=mean(Survived))->grp_stats
 
-names_characterized<-as.character(names[40])
-splitted_name<-strsplit(names_characterized,",")
-list5<-splitted_name[[1]]
-list6<-list5[1]
-splitted_names[40]<-list6
-splitted_names[40]
+#extract title 
+mydata$Title <- gsub('(.*, )|(\\..*)', '', mydata$Name)
+table(mydata$Title)
+rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
+                'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
+
+mydata$Title[mydata$Title == 'Mlle']        <- 'Miss' 
+mydata$Title[mydata$Title == 'Ms']          <- 'Miss'
+mydata$Title[mydata$Title == 'Mme']         <- 'Mrs' 
+mydata$Title[mydata$Title %in% rare_title]  <- 'Rare Title'
+
+#extract surname
+mydata$Surname <- sapply(mydata$Name, function(x) strsplit(as.character(x), split = '[,.]')[[1]][1])
+
+#check same surname within grp0
+tmp<-mydata[mydata$grp==0,]
+tmp_name<-table(tmp$Surname)
+tmp_name<-tmp_name[tmp_name>1]
+tmp[tmp$Surname==names(tmp_name)[2],]
+tmp2<-mydata[mydata$Surname==names(tmp_name)[2],]#interesting case to tell one entry might be entered wrong
